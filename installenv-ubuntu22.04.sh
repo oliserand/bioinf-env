224f48104fb06e37cbb74bced3343e7f80e7be0e
Olivier <oliserand@gmail.com>
2024-02-01 14:23:29 +0200

    Update installenv-ubuntu22.04.sh
    Few more automations for apt

--- installenv-ubuntu22.04.sh
+++ installenv-ubuntu22.04.sh
@@ -44,11 +44,11 @@ echo "LESS+='-M'" >> /home/${username}/.bashrc
 
 # Install R
 apt update -qq
-apt install --no-install-recommends software-properties-common dirmngr
+apt install -y --no-install-recommends software-properties-common dirmngr
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
-add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
-apt install --no-install-recommends r-base
-add-apt-repository ppa:c2d4u.team/c2d4u4.0+
+add-apt-repository -y "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+apt install -y --no-install-recommends r-base
+add-apt-repository -y ppa:c2d4u.team/c2d4u4.0+
 
 # Install RStudio
 cd $sourcedir 
